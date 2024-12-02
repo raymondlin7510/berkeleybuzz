e0dadac018c31d21355a26676c4152da8c9544f4
Raymond <raymond.lin7510@berkeley.edu>
2024-12-01 18:40:32 -0800

    Made the Organization Card

---
+++ frontend/src/components/OrgCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/OrgCard.css';
+
+interface OrgCardProps {
+    logo: string;
+    name: string;
+  }
+  
+  const OrgCard: React.FC<OrgCardProps> = ({ logo, name }) => {
+    return (
+      <div className="wrapper">
+        <img src={logo} />
+        <h2 className="orgTitle">{name}</h2>
+      </div>
+    );
+  };
+
+export default OrgCard;
--- frontend/src/pages/EventSearch.tsx
+++ frontend/src/pages/EventSearch.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import '../styles/EventSearch.css';
-import '../components/SearchBar';
-import '../components/EventCard';
+import SearchBar from '../components/SearchBar';
+import EventCard from '../components/EventCard';
+import OrgCard from '../components/OrgCard';
   
 const EventSearch = () => {
   return (
     <div className="wrapper">
-      
+      <img src="" />
+      <div className="backgroundImage">
+          <div className="backgroundSearch">
+              <SearchBar />
+          </div>
+      </div>
+      <div className="events">
+        <h2>Events</h2>
+        
+      </div>
+      <div className="orgs">
+        <h2>Organizations</h2>
+
+      </div>
     </div>
   );
 };
